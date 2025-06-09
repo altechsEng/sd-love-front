e135029672f6e4dba1d1b8376399825d027ba037
christnloga <christ.nloga@gmail.com>
2025-06-09 14:13:08 +0100

    Added config for animation

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import "./global.css";
 import { StyleSheet, Text, View } from 'react-native';
 import Application from './app/_layout';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
 	return (
-		// <SafeAreaView>
+		<GestureHandlerRootView style={{ flex: 1 }}>
 			<Application />
-		// </SafeAreaView>
+		</GestureHandlerRootView>
 	);
 }
 
