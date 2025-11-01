6e37b47e62bbe44e32680158e956952f04d992eb
christnloga <christ.nloga@gmail.com>
2025-11-01 10:31:51 +0100

    Added extra dependancies

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
-import { Text, View } from "react-native";
-import React, { Component } from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Text, useColorScheme, View } from "react-native";
+import React, { Component, useEffect } from "react";
 import { Stack } from "expo-router";
 import "../../global.css";
 import { StatusBar } from "expo-status-bar";
@@ -8,13 +9,61 @@ import { GlobalVariableProvider } from "../context/global";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AuthProvider, useAuth } from "@/src/context/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import { DefaultTheme } from "@react-navigation/native";
+import * as SplashScreen from "expo-splash-screen";
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: "(tabs)",
+};
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const isLoggedIn = false;
+  const [loaded, error] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
   const queryClient = new QueryClient();
 
   return (
-    <React.Fragment>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <GestureHandlerRootView>
         <GlobalVariableProvider>
           <AuthProvider>
@@ -32,6 +81,6 @@ export default function RootLayout() {
           </AuthProvider>
         </GlobalVariableProvider>
       </GestureHandlerRootView>
-    </React.Fragment>
+    </ThemeProvider>
   );
 }
